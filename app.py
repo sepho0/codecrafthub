ce99e518207ca09b7b56a944379043cb6acf586e
sepho0 <rosekainasephoraaltidor7@gmail.com>
2026-04-08 09:25:18 -0400

    index.html

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ from flask import Flask, jsonify, request, abort
 import json
 import os
 from datetime import datetime
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 DATA_FILE = 'courses.json'
 
 # --- FONCTIONS D'AIDE (Helper Functions) ---
